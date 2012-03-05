c66cd5a032b7e899317ee18682093d615d1e25ce
Lopo <lopo@losys.eu>
2012-03-05 08:28:07 +0100

    NotORM DataSource

---
+++ DataSources/NotORM/NotORM.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace DataGrid\DataSources\NotORM;
+
+use DataGrid\DataSources\IDataSource,
+	NotORM_Result;
+
+/**
+ * @author Lopo <lopo@losys.eu>
+ * based on NetteDB version 
+ */
+class NotORM
+extends \DataGrid\DataSources\Mapped
+{
+	/** @var NotORM_Result */
+	private $result;
+
+	/** @var array fetched data */
+	private $data;
+
+	/** @var int Total data count */
+	private $count;
+
+
+	/**
+	 * @param \NotORM_Result $result
+	 */
+	public function __construct(\NotORM_Result $result)
+	{
+		$this->result = $result;
+	}
+
+	/**
+	 * @param array $mapping
+	 */
+	public function setMapping(array $mapping)
+	{
+		parent::setMapping($mapping);
+		$selects=array();
+		foreach ($mapping as $k => $m) {
+			$selects[]="$m AS `$k`";
+		}
+		$this->result->select(implode(',', $selects));
+	}
+
+	/**
+	 * Add filtering onto specified column
+	 *
+	 * @param string $column column name
+	 * @param string $operation filter
+	 * @param string|array $value operation mode
+	 * @param string $chainType chain type (if third argument is array)
+	 * @return NotORM (fluent)
+	 * @throws \Nette\InvalidArgumentException
+	 */
+	public function filter($column, $operation = IDataSource::EQUAL, $value = NULL, $chainType = NULL)
+	{
+		$col = $column;
+		if ($this->hasColumn($column)) {
+			$col = $this->mapping[$column];
+		}
+
+		if (is_array($operation)) {
+			if ($chainType !== self::CHAIN_AND && $chainType !== self::CHAIN_OR) {
+				throw new \Nette\InvalidArgumentException('Invalid chain operation type.');
+			}
+		} else {
+			$operation = array($operation);
+		}
+
+		if (empty($operation)) {
+			throw new \Nette\InvalidArgumentException('Operation cannot be empty.');
+		}
+
+		$conds = array();
+		$values = array();
+		foreach ($operation as $o) {
+			$this->validateFilterOperation($o);
+
+			$c = "$col $o";
+			if ($o !== self::IS_NULL && $o !== self::IS_NOT_NULL) {
+				$c .= ' ?';
+
+				$values[] = ($o === self::LIKE || $o === self::NOT_LIKE)
+					? \DataGrid\DataSources\Utils\WildcardHelper::formatLikeStatementWildcards($value)
+					: $value;
+			}
+
+			$conds[] = $c;
+		}
+
+		$conds = implode(" ( $chainType ) ", $conds); // "(cond1) OR (cond2) ..."  -- outer braces missing for now
+		$this->result->where("( $conds )", $values);
+
+		return $this;
+	}
+
+	/**
+	 * Adds ordering to specified column
+	 *
+	 * @param string $column column name
+	 * @param string $order one of ordering types
+	 * @return NotORM (fluent)
+	 */
+	public function sort($column, $order = IDataSource::ASCENDING)
+	{
+		if (!$this->hasColumn($column)) {
+			$this->result->order($column . ' ' . ($order === self::ASCENDING ? 'ASC' : 'DESC'));
+		}
+		else {
+			$this->result->order($this->mapping[$column] . ' ' . ($order === self::ASCENDING ? 'ASC' : 'DESC'));
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Reduce the result starting from $start to have $count rows
+	 *
+	 * @param int $count the number of results to obtain
+	 * @param int $start the offset
+	 * @return NotORM (fluent)
+	 * @throws \Nette\OutOfRangeException
+	 */
+	public function reduce($count, $start = 0)
+	{
+		// Delibearately skipping check agains count($this)
+
+		if ($count === NULL) {
+			$count = 0;
+		}
+		if ($start === NULL) {
+			$start = 0;
+		}
+
+		if ($start < 0 || $count < 0) {
+			throw new \Nette\OutOfRangeException;
+		}
+
+		$this->result->limit($count, $start);
+		return $this;
+	}
+
+	/**
+	 * Get iterator over data source items
+	 *
+	 * @return \ArrayIterator
+	 */
+	public function getIterator()
+	{
+		return $this->result;
+	}
+
+	/**
+	 * Fetches and returns the result data.
+	 *
+	 * @return array
+	 */
+	public function fetch()
+	{
+		throw $this->result->fetch();
+	}
+
+	/**
+	 * Count items in data source
+	 *
+	 * @return int
+	 */
+	public function count()
+	{
+		$query = clone $this->result;
+		$this->count = $query->count();
+
+		return $this->count;
+	}
+
+	/**
+	 * Return distinct values for a selectbox filter
+	 *
+	 * @param string $column Column name
+	 * @return array
+	 */
+	public function getFilterItems($column)
+	{
+		$query = clone $this->result;
+		return $query->select($column)->group($column)->fetchPairs($column, $column);
+	}
+
+	/**
+	 * Clone instance
+	 */
+	public function __clone()
+	{
+		$this->result = clone $this->result;
+	}
+}
+
